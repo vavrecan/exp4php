cbaac4305b8239806cdf7b08b27bea178e74ccb5
Marek Vavrečan <vavrecan@gmail.com>
2014-09-29 22:28:49 +0200

    simplified index

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <?php
 
 require_once("exp4php/ExpressionBuilder.php");
-require_once("exp4php/func/Func.php");
 
-use exp4php\ExpressionBuilder;
-use exp4php\func\Func;
-
-class CustomFunction extends Func {
+class CustomFunction extends exp4php\func\Func {
     public function apply($args) {
         return -1;
     }
 }
 
 // setup
-$e = new ExpressionBuilder("sum(4,2,3,avg(5,3))+custom(2,q1)");
+$e = new exp4php\ExpressionBuilder("sum(4,2,3,avg(5,3))+custom(2,q1)");
 $e->variable("q1");
 $e->func(new CustomFunction("custom"));
 $build = $e->build();
